bce2dc392a4a4e5d8d5ac634cbe1308c1edcb311
Luke Gloege <lukegloege@gmail.com>
2025-06-12 20:35:27 -0400

    cli: start of CLI to import datasets. Tested it, I know it works :)

---
+++ cli/README.md
@@ -0,0 +1,22 @@
+# Importer CLI
+
+This contains a [typer](https://typer.tiangolo.com/)-based CLI to import datasets into the database.
+This is a work in progress.
+
+syntax:
+
+```sh
+importer SOURCE
+```
+
+for example, if you want to import `iso-3166-1` country names, then you run:
+
+```sh
+importer iso-3166-1
+```
+
+## Notes
+- May want to think about choice of what happens `ON CONFLICT`
+- All data imports sequentially. Add an option to import an entire dataset at once using the `COPY` command in postgres.
+- Have an option scrub data database. Useful when we update datasources. Have this be a different CLI `importer` is just for ingesting data into the database and `scrubber` is to remove data from the database. 
+- consider a `db-stats` CLI that lists datasources in each table?
---
+++ cli/main.py
@@ -0,0 +1,81 @@
+import csv
+from pathlib import Path
+
+import psycopg
+from psycopg import Cursor
+from rich import print
+from sqlmodel import SQLModel
+import typer
+
+import models
+
+# this the default database url if one is not specified
+# this is just running postgres on localhost
+DEFAULT_URL = "postgresql://postgres:postgres@127.0.0.1:5432/ghgtracker"
+
+app = typer.Typer()
+
+
+def clean_record(record):
+    """conerts empty strings to None"""
+    for key, value in record.items():
+        if value in ("", "null"):
+            record[key] = None
+    return record
+
+
+def insert_record(curs: Cursor, table: str, pkey: str, record: dict):
+    """insert a single record into the the database"""
+    
+    columns = list(record.keys())
+    values = [record[col] for col in columns]
+
+    placeholders = ", ".join(["%s"] * len(values))
+    column_names = ", ".join([f'"{col}"' for col in columns])
+
+    # on a primary key conflict we are forcing the update
+    # may want to think about this choice ...
+    query = f"""
+    INSERT INTO "{table}" ({column_names})
+    VALUES ({placeholders})
+    ON CONFLICT ("{pkey}") DO UPDATE SET updated_at = NOW();
+    """
+
+    curs.execute(query, values)
+
+
+def insert_from_csv(csv_path, table, database_url, pkey="id"):
+    """sequentially insert into the database from a csv file"""
+    with psycopg.connect(database_url) as conn:
+        with conn.cursor() as curs:
+            with open(csv_path, newline="") as csvfile:
+                reader = csv.DictReader(csvfile)
+                for record in reader:
+                    clean_record(record)
+                    insert_record(curs, table, pkey, record)
+
+            conn.commit()
+
+@app.command()
+def sequential(source: str, url: str = DEFAULT_URL):
+    # all tables in dependcy order
+    sorted_tables = [table.name for table in SQLModel.metadata.sorted_tables]
+
+    # list files in datasource in depdency order
+    path = Path(f"./data/{source}/processed")
+
+    print(f"[green]importing {source} from {path}")
+    
+    csv_files = [p.stem for p in path.glob("*.csv")]
+    tables_to_import = [table for table in sorted_tables if table in csv_files]
+
+    for table in tables_to_import:
+        print(f"[green]    populating the {table} table...")
+        csv_file = path / f"{table}.csv"
+        insert_from_csv(csv_file, table, url, pkey="id")
+
+    print(f"[green]\nSuccessfully imported {source} into the database !!\n")
+
+
+if __name__ == "__main__":
+    app()
